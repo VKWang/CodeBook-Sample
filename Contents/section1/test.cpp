d05b5b272a82ae63628b9facc7d1076d9183e9d9
VKWang <VKWang914@gmail.com>
2022-09-23 21:05:17 +0800

    新增test.cpp+修改content.tex

---
+++ Contents/section1/test.cpp
@@ -0,0 +1,20 @@
+// map::begin/end
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main ()
+{
+  map<char,int> mymap;
+
+  mymap['b'] = 100;
+  mymap['a'] = 200;
+  mymap['c'] = 300;
+
+  // show content:
+  for (map<char,int>::iterator it = mymap.begin(); it != mymap.end(); ++it)
+    cout << it->first << " => " << it->second << '\n';
+
+  return 0;
+}
