f3491173036c2c676515d7f8a8ebbaaa3d38b74c
VKWang <VKWang914@gmail.com>
2022-10-15 00:13:42 +0800

    修剪 algorithm.cpp

--- Contents/Libraries/algorithm.cpp
+++ Contents/Libraries/algorithm.cpp
@@ -2,44 +2,25 @@
 using namespace std;
 
 int main() {
-    
-    void sort (RandomAccessIterator first, RandomAccessIterator last);
-    /*  Sorts the elements in the range [first,last) into ascending order
-     *  In N*lg(N) complexity
-     */
+    // FI(ForwaradIterator)
+    // RAI(RandomAccessIterator)
+    // BI(BidirectionalIterator)
+    void sort(RAI first, RAI last);
 
-    ForwardIterator lower_bound (ForwardIterator first, ForwardIterator last, const T& val);
-    /*  Returns an iterator pointing to the first element in the range [first,last) which is >= val
-     *  In lg(N)+1 complexity
-     *      requires sorted elements
-     */
+    FI lower_bound(FI first, FI last, const T& k);
+    /* 最左邊 ≥ k 的位置 */
 
-    ForwardIterator upper_bound (ForwardIterator first, ForwardIterator last, const T& val);
-    /*  Returns an iterator pointing to the first element in the range [first,last) which is > val
-     *  In lg(N)+1 complexity
-     *      requires sorted elements
-     */
+    FI upper_bound(FI first, FI last, const T& k);
+    /* 最左邊 > k 的位置 */
     
-    pair<ForwardIterator,ForwardIterator> equal_range (ForwardIterator first, ForwardIterator last, const T& val);
-    /*  Returns the bounds of the subrange with all the elements == val of the range [first,last)
-     *      return type equivalent to pair< lower_bound(), upper_bound>
-     *  In 2*lg(N)+1 complexity
-     *      requires sorted elements
-     */
+    pair<FI,FI> equal_range(FI first, FI last, const T& k);
+    /* 等於 k 的範圍 [lower_bound, upper_bound) */
     
-    bool next_permutation (BidirectionalIterator first, BidirectionalIterator last);
-    /*  Rearranges the elements in the range [first,last) into the next lexicographically greater permutation, then returns
-     *      true if could rearrange as a lexicographicaly greater permutation
-     *      false if no greater arrangement than the previous (and sorted in ascending order)
-     *  In N/2 complexity
-     */
+    bool next_permutation(BI first, BI last);
+    /* 使用已經排序(由小到大)的資料，產生下一組排列 */
 
-    bool prev_permutation (BidirectionalIterator first, BidirectionalIterator last);
-    /*  Rearranges the elements in the range [first,last) into the previous lexicographically-ordered permutation, then returns
-     *      true if could rearrange as a lexicographicaly smaller permutation
-     *      false if arrangement is the largest possible (and sorted in descending order)
-     *  In N/2 complexity
-    */
+    bool prev_permutation(BI first, BI last);
+    /* 針對已經「逆向」排序(由大到小)的資料，產生上一組排序 */
 
     return 0;
 }
