af765b4ef74dd829bd9203e1d9d100fb3e10d8aa
VKWang <VKWang914@gmail.com>
2022-10-15 01:18:31 +0800

    修剪cstdib.cpp

--- Contents/Libraries/cstdlib.cpp
+++ Contents/Libraries/cstdlib.cpp
@@ -2,95 +2,34 @@
 using namespace std;
 
 int main() {
-
-    // Functions
-        // String conversion
-            double atof (const char* str);  // Convert string to double; return 0.0 if no conversion
-            int atoi (const char * str);    // Convert string to integer;
-            long int atol ( const char * str ); // Convert string to long integer; return 0 if no conversion
-            long long int atoll ( const char * str );   // Convert string to long long integer; return 0 if no conversion
-            double strtod (const char* str, char** endptr); // Convert string to double; return 0.0 if no conversion, HUGE_VAL(cmath) if out of range
-            float strtof (const char* str, char** endptr);  // Convert string to float
-
-strtol
-    Convert string to long integer (function)
-
-strtold
-    Convert string to long double (function)
-
-strtoll
-    Convert string to long long integer (function)
-
-strtoul
-    Convert string to unsigned long integer (function)
-
-strtoull
-    Convert string to unsigned long long integer (function)
-
-
-Pseudo-random sequence generation
-
-rand
-    Generate random number (function)
-
-srand
-    Initialize random number generator (function)
-
-
-Searching and sorting
-
-bsearch
-    Binary search in array (function)
-
-qsort
-    Sort elements of array (function)
-
-
-Integer arithmetics
-
-abs
-    Absolute value (function)
-
-div
-    Integral division (function)
-
-labs
-    Absolute value (function)
-
-ldiv
-    Integral division (function)
-
-llabs
-    Absolute value (function)
-
-lldiv
-    Integral division (function)
-
-
-
-Macro constants
-
-NULL
-    Null pointer (macro)
-
-RAND_MAX
-    Maximum value returned by rand (macro)
-
-
-
-Types
-
-div_t
-    Structure returned by div (type)
-
-ldiv_t
-    Structure returned by ldiv (type)
-
-lldiv_t
-    Structure returned by lldiv (type)
-
-size_t
-    Unsigned integral type (type)
+// Function: String conversion
+    double atof(const char* str);
+        // Convert string to double
+    int atoi(const char * str);
+        // Convert string to integer
+    long int atol(const char * str);
+        // Convert string to long integer
+    long long int atoll(const char * str);
+        // Convert string to long long integer
+    double strtod(const char* str, char** endptr);
+        // Convert string to double;
+    float strtof(const char* str, char** endptr);
+        // Convert string to float
+    long int strtol(const char* str, char** endptr, int base);
+        // Convert string to long integer
+    long double strtold(const char* str, char** endptr);
+        // Convert string to long double
+    long long int strtoll(const char* str, char** endptr, int base);
+        // Convert string to long long integer
+    unsigned long int strtoul(const char* str, char** endptr, int base);
+        // Convert string to unsigned long intege
+    unsigned long long int strtoull(const char* str, char** endptr, int base);
+        // Convert string to unsigned long long integer
+// Function: Integer arithmetics
+    int abs(int n);
+    long int llabs(long int n);
+    long long int llabs(long long int n);
+        // Absolute value
 
 
     return 0;
